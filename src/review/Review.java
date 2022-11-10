3af51ce8672bd5293e925a2031c7139765e13ad1
tekRashed <rashed.delbary@tekschool.us>
2022-11-09 20:14:31 -0500

    in review

--- src/review/Review.java
+++ src/review/Review.java
@@ -3,8 +3,11 @@ package review;
 public class Review {
 
 	public static void main(String[] args) {
+	}
 		// TODO Auto-generated method stub
-
+public void test() {
+	System.out.println("this is for review session");
+}
 	}
 
-}
+
